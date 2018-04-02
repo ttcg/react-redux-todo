059efcb1016492e913dfd6b11652776cb1354bf9
Thet Wai <thetwai2000@gmail.com>
2018-04-02 23:14:02 +0100

    introduced CombineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,8 @@
-import {ADD_TODO_ITEM, REMOVE_TODO_ITEM} from '../constants/action-types';
+import { combineReducers } from 'redux';
+import todo from './todoReducer';
 
-const initialState = {
-  tasks: ['thet', 'wai']
-};
-
-const rootReducer = (state = initialState, action) => {   
-    switch (action.type) {
-    case ADD_TODO_ITEM:
-        return { ...state, tasks: [...state.tasks, action.payload] };
-    case REMOVE_TODO_ITEM: {
-        var newList = state.tasks.filter((elem, index, arr) => arr.indexOf(elem) !== action.payload);
-        return {...state, tasks: newList };
-    }
-    default:
-        return state;
-    }
-}
-
+const rootReducer = combineReducers({
+    tasks: todo
+});
 
 export default rootReducer;
---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,21 @@
+import {ADD_TODO_ITEM, REMOVE_TODO_ITEM} from '../constants/action-types';
+
+const initialState = {
+  tasks: ['thet', 'wai']
+};
+
+const todoReducer = (state = initialState, action) => {   
+    switch (action.type) {
+    case ADD_TODO_ITEM:
+        return { ...state, tasks: [...state.tasks, action.payload] };
+    case REMOVE_TODO_ITEM: {
+        var newList = state.tasks.filter((elem, index, arr) => arr.indexOf(elem) !== action.payload);
+        return {...state, tasks: newList };
+    }
+    default:
+        return state;
+    }
+}
+
+
+export default todoReducer;
