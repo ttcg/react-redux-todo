c20368b8d5e70b370ef4c92e58e9c89ebf46d2ac
T1\Thet.Wai <thet.wai@traceone.com>
2018-04-06 16:41:29 +0100

    Footer Component added

---
+++ src/components/common/footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Moment from 'react-moment';
+import pkg from '../../../package.json';
+
+import {
+    Container
+} from 'reactstrap';
+
+class footer extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <hr style={{marginTop: 100}}/>
+                <Container>
+                    <p className="float-right"><small>v {pkg.version}</small></p>
+                    <p>&copy; <Moment format='YYYY' /> TTCG</p>
+                </Container>
+            </React.Fragment>
+        );
+    }
+}
+
+export default footer;
