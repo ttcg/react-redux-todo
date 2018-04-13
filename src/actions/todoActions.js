512cf353a5c706c151aac777c3823a525d4aed5e
T1\Thet.Wai <thet.wai@traceone.com>
2018-04-13 17:02:17 +0100

    added ADD_TODO_ITEM

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,12 +1,13 @@
 import * as actionTypes from '../actions/actionTypes';
 import TodoService from '../services/TodoService';
 
+//export const addTodoItem = () => ({ type: actionTypes.ADD_TODO_ITEM });
 
-export const addTodoItemSuccess = todoItem => ({ type: actionTypes.ADD_TODO_ITEM_SUCCESS, payload: true });
+export const addTodoItemSuccess = todoItem => ({ type: actionTypes.ADD_TODO_ITEM_SUCCESS });
 
 export const loadTodoItemsSuccess = items => ({ type: actionTypes.LOAD_TODO_SUCCESS, payload: items });
 
-export const removeTodoItem = index => ({ type: actionTypes.REMOVE_TODO_ITEM, payload: index });
+export const removeTodoItemSuccess = () => ({ type: actionTypes.REMOVE_TODO_ITEM });
 
 export const loadTodoItems = () => {
     return function (dispatch) {
@@ -20,6 +21,14 @@ export const addTodoItem = (item) => {
     return function (dispatch) {
         return TodoService.add(item).then(item => {
             dispatch(addTodoItemSuccess(item));
+        });        
+    };
+}
+
+export const removeTodoItem = (id) => {
+    return function (dispatch) {
+        return TodoService.remove(id).then(item => {
+            dispatch(removeTodoItemSuccess());
         });        
     };
 }
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -3,14 +3,13 @@ import * as actionTypes from '../actions/actionTypes';
 const todoReducer = (state = [], action) => {
     switch (action.type) {
         case actionTypes.ADD_TODO_ITEM_SUCCESS: {
-            return { addTodoSuccess: action.payload };
+            return { ...state, addTodoSuccess: true };
         }
-        case actionTypes.REMOVE_TODO_ITEM: {
-            var newList = state.filter((elem, index, arr) => arr.indexOf(elem) !== action.payload);
-            return { ...state, tasks: newList };
+        case actionTypes.REMOVE_TODO_ITEM: {            
+            return { ...state, removeTodoSuccess: true };
         }
         case actionTypes.LOAD_TODO_SUCCESS:
-            return { tasks: action.payload };
+            return { ...state, tasks: action.payload };
         default:
             return state;
     }
