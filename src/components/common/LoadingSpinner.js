7ada53bae7573abe25f8cd511126b2e467d01fe9
Thet Wai <thetwai2000@gmail.com>
2018-05-17 23:00:49 +0100

    Added React-Delay

--- src/components/common/LoadingSpinner.js
+++ src/components/common/LoadingSpinner.js
@@ -1,15 +1,15 @@
 import React from 'react';
-//import Delay from 'react-delay';
+import Delay from 'react-delay';
 import './LoadingSpinner.css';
 
 const LoadingSpinner = () =>  (
-    //<Delay wait={1000}>
+    <Delay wait={300}>
         <div className="modal-loading">
             <div className="modal-loading-content">
                 <i className="fa fa-spinner fa-spin" /> Loading...
             </div>
         </div>
-    //</Delay>
+    </Delay>
 );
 
 export default LoadingSpinner;
