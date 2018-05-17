f1112631117b258f21cce8cca25f218398507523
Thet Wai <thetwai2000@gmail.com>
2018-05-17 23:00:27 +0100

    Updated new Api Url

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,7 +4,7 @@ const About = () => {
 	return (		
 		<React.Fragment>			
 			<p>Demo website which uses React and Redux technology.  It contains basic CRUD activities.</p>
-			<p>It uses the .Net Core services hosted on GCP and the service can be viewed <a href="http://35.195.110.121/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+			<p>It uses the .Net Core services hosted on GCP and the service can be viewed <a href={process.env.REACT_APP_ServiceUrl} target="_blank" rel="noopener noreferrer">here</a>.</p>
 			<p>This app uses the following libraries:</p>
 			<ul>
 				<li>Axios</li>
